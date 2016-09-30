112ed96b6ef88ac2f08a281a79f02051602fae9d
yt <yt@yt@debyt.com>
2016-09-30 19:18:34 +0800

    按照更新说明更改了FlaskForm

--- app/forms.py
+++ app/forms.py
@@ -1,15 +1,15 @@
 from flask import Flask
 from app import app
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import TextField, BooleanField
 from wtforms.validators import Required
 from wtforms import StringField, PasswordField,TextAreaField
 from wtforms.validators import DataRequired
 
-class UsernamePasswordForm(Form):
+class UsernamePasswordForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
 
-class RecordForm(Form):
+class RecordForm(FlaskForm):
     time = StringField('time', validators=[DataRequired()])
     record = TextAreaField('record', validators=[DataRequired()])
